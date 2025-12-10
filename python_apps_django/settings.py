a9036e57f54972666a98fefd5a19e8cc4e30201a
rikarikarinn <nazuki_shibata@icloud.com>
2025-12-10 23:11:42 +0900

    .

--- python_apps_django/settings.py
+++ python_apps_django/settings.py
@@ -135,7 +135,12 @@ USE_TZ = True
 # STATIC / MEDIA
 # ============================================================
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
